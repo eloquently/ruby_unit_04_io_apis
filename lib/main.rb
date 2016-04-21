4731ab50051e2948d3cf31b99060b88e1a0405dc
etmckay1 <ericmckay4@gmail.com>
2016-04-21 09:02:17 +0000

    main.rb changes

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,13 @@
+# This program will run your restaurant recommender
+# to run it execute the following command in your bash terminal:
+# ruby lib/main.rb
+
+require_relative 'restaurant_recommender'
 rr = RestaurantRecommender.new()
 
 puts "Welcome to restaurant recommender"
 while true
+    print ' > '
     input = gets.chomp
     break if input == "quit"
 
